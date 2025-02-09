9203d14b99dcdd4a20401d17294ccfe41aa4f065
YORY31 <20230272@itla.edu.do>
2025-02-09 13:02:56 -0400

    Dia 1

---
+++ Retos/Day1.js
@@ -0,0 +1,35 @@
+
+
+//Desafio resuelto 
+let numUno = 1;
+let strUno = "1";
+let numTrenta = 30;
+let strTrenta = "30";
+let numDiez = 10;
+let strDiez = "10";
+
+if (numUno == strUno) {
+  console.log(
+    `Las variables ${numUno} y ${strUno} tienen el mismo valor, pero tipos diferentes`
+  );
+} else {
+  console.log(`Las variables ${numUno} y ${strUno} no tienen el mismo valor`);
+}
+
+if (numTrenta === strTrenta) {
+  console.log(
+    `Las variables ${numTrenta} y ${strTrenta} tienen el mismo valor, y el mismo tipo`
+  );
+} else {
+  console.log(
+    `Las variables ${numTrenta} y ${strTrenta} no tienen el mismo tipo`
+  );
+}
+
+if (numDiez == strDiez) {
+  console.log(
+    `Las variables ${numDiez} y ${strDiez} tienen el mismo valor, pero tipos diferentes`
+  );
+} else {
+  console.log(`Las variables ${numDiez} y ${strDiez} no tienen el mismo valor`);
+}
