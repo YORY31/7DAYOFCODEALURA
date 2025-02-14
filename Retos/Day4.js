f0d59b5da9de700e3df07de3c836122cfe1f11b6
YORY31 <20230272@itla.edu.do>
2025-02-14 19:45:27 -0400

    new files and new update day 4

--- Retos/Day4.js
+++ Retos/Day4.js
@@ -3,17 +3,17 @@ numeroSecreto = Math.floor(Math.random() * 10) + 1;
 intentos = 3;
  
 while (intentos > 0) {
-    let usuario = parseInt(prompt("Adivina el número entre 0 y 10:"));
+    let usuario = parseInt(prompt("Adivina el numero entre 0 y 10:"));
 
     if (usuario === numeroSecreto) {
-        alert("¡Felicidades! Has adivinado el número.");
+        alert("¡Felicidades! Has adivinado el numero.");
         break;
     } else {
         intentos--;
         if (intentos > 0) {
             alert(`Numero incorrecto. Te quedan ${intentos} intento(s).`);
         } else {
-            alert(`Lo siento, has agotado tus intentos. El número era ${numeroSecreto}.`);
+            alert(`Lo siento, has agotado tus intentos. El numero era ${numeroSecreto}.`);
         }
     }
 }
